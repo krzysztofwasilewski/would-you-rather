3c249c7f9fca22fdd691de2f74d5b8f8116509e0
krzysztofwasilewski <krzysztofwasilewski76@gmail.com>
2020-05-10 16:01:36 +0200

    feat: Add `Navigation` component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,2 +1,22 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import {NavLink} from 'react-router-dom';
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink>Home</NavLink>{' '}
+        </li>
+        <li>
+          <NavLink>New Poll</NavLink>{' '}
+        </li>
+        <li>
+          <NavLink>Leaderboard</NavLink>{' '}
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
