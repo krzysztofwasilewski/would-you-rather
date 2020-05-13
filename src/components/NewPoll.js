81b31c540f7cae2675b690ed765f3e93a1931b2d
krzysztofwasilewski <krzysztofwasilewski76@gmail.com>
2020-05-14 01:08:35 +0200

    feat: Implement new poll form

--- src/components/NewPoll.js
+++ src/components/NewPoll.js
@@ -1,25 +1,69 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {handleAddQuestion} from '../actions/questions';
+import {values, isEmpty, pipe, any} from 'ramda';
 
 class NewPoll extends Component {
   state = {optionOneText: '', optionTwoText: ''};
+  handleTextChange = option => e => {
+    const val = e.target.value;
+    e.preventDefault();
+    this.setState({[option]: val});
+  };
+  handleSubmitForm = e => {
+    e.preventDefault();
+    this.props.handleAddQuestion({
+      author: this.props.authedUser,
+      ...this.state
+    });
+    this.props.history.push('/');
+  };
   render() {
     return (
       <div>
-        <form>
-          <ul>
-            <li>
-              <input type='text' value={this.state.optionOneText} />
-            </li>
-            <li>
-              <input type='text' value={this.state.optionTwoText} />
-            </li>
-          </ul>
-          <button>Submit</button>
+        <form onSubmit={this.handleSubmitForm}>
+          <fieldset>
+            <legend>Would you rather:</legend>
+            <ul>
+              <li>
+                <input
+                  type='text'
+                  autoFocus
+                  value={this.state.optionOneText}
+                  onChange={this.handleTextChange('optionOneText')}
+                />
+              </li>
+              <li>
+                <input
+                  type='text'
+                  value={this.state.optionTwoText}
+                  onChange={this.handleTextChange('optionTwoText')}
+                />
+              </li>
+            </ul>
+            <button disabled={pipe(values, any(isEmpty))(this.state)}>
+              Submit
+            </button>
+          </fieldset>
         </form>
       </div>
     );
   }
 }
 
-export default NewPoll;
+NewPoll.propTypes = {
+  authedUser: PropTypes.string.isRequired,
+  handleAddQuestion: PropTypes.func.isRequired,
+  history: PropTypes.shape({push: PropTypes.func.isRequired}).isRequired
+};
+
+function mapStateToProps({authedUser}) {
+  return {authedUser};
+}
+
+const mapDispatchToProps = {
+  handleAddQuestion
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewPoll);
