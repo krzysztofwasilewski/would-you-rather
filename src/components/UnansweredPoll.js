4baba38579df3cf1488ba8921709918cf48a63b9
krzysztofwasilewski <krzysztofwasilewski76@gmail.com>
2020-05-14 01:09:12 +0200

    feat: Implement handlin gof unanswered polls

---
+++ src/components/UnansweredPoll.js
@@ -0,0 +1,78 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {pick} from 'ramda';
+import {connect} from 'react-redux';
+
+import {saveAnswer} from '../actions/shared';
+
+class UnansweredPoll extends Component {
+  state = {value: ''};
+  handleChange = e => this.setState({value: e.target.value});
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.saveAnswer(
+      this.props.authedUser,
+      this.props.id,
+      this.state.value
+    );
+  };
+  render() {
+    const {name, avatarURL, optionOneText, optionTwoText} = this.props;
+    return (
+      <div>
+        <h2>{`${name} asks:`}</h2>
+        <div>
+          <img src={avatarURL}></img>
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          <fieldset>
+            <legend>Would you rather…</legend>
+            <ul>
+              {[
+                ['optionOne', optionOneText],
+                ['optionTwo', optionTwoText]
+              ].map(([option, text]) => (
+                <li key={option}>
+                  <label>
+                    <input
+                      type='radio'
+                      value={option}
+                      checked={this.state.value === option}
+                      onChange={this.handleChange}
+                    />
+                    {text}
+                  </label>
+                </li>
+              ))}
+            </ul>
+            <button disabled={!this.state.value}>Submit</button>
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
+}
+
+UnansweredPoll.propTypes = {
+  name: PropTypes.string.isRequired,
+  avatarURL: PropTypes.string.isRequired,
+  optionOneText: PropTypes.string.isRequired,
+  optionTwoText: PropTypes.string.isRequired,
+  authedUser: PropTypes.string.isRequired,
+  saveAnswer: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired
+};
+
+function mapStateToProps({users, questions, authedUser}, {id}) {
+  return {
+    ...pick(['name', 'avatarURL'], users[questions[id].author]),
+    optionOneText: questions[id].optionOne.text,
+    optionTwoText: questions[id].optionTwo.text,
+    authedUser
+  };
+}
+
+const mapActionsToProps = {
+  saveAnswer
+};
+export default connect(mapStateToProps, mapActionsToProps)(UnansweredPoll);
--- src/components/UnansweredPollList.js
+++ src/components/UnansweredPollList.js
@@ -1,10 +1,49 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import {
+  complement,
+  contains,
+  filter,
+  map,
+  pipe,
+  prop,
+  propSatisfies,
+  sortBy,
+  values,
+  where
+} from 'ramda';
+import React from 'react';
+import {connect} from 'react-redux';
+import UnansweredPoll from './UnansweredPoll';
+const UnansweredPollList = ({questionIds}) => (
+  <div>
+    {questionIds.map(id => (
+      <UnansweredPoll key={id} id={id} />
+    ))}
+  </div>
+);
 
-const UnansweredPollList = () => {
-    return <div>
-        Unanswered poll List
-    </div>
+UnansweredPollList.propTypes = {
+  questionIds: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+function mapStateToProps({authedUser, questions}) {
+  const doesntContainUser = complement(contains(authedUser));
+  const optionDoesntContainUser = propSatisfies(doesntContainUser, 'votes');
+  const questionDoesntContainUser = where({
+    optionOne: optionDoesntContainUser,
+    optionTwo: optionDoesntContainUser
+  });
+  const userFilter = filter(questionDoesntContainUser);
+  const sortByTimestamp = sortBy(prop('timestamp'));
+
+  return {
+    questionIds: pipe(
+      userFilter,
+      values,
+      sortByTimestamp,
+      map(prop('id'))
+    )(questions)
+  };
 }
 
-export default UnansweredPollList;
+export default connect(mapStateToProps)(UnansweredPollList);
