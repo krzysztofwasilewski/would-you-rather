9dffc7ec740d9de8e09fdd5519d06a68216ff742
krzysztofwasilewski <krzysztofwasilewski76@gmail.com>
2020-05-14 01:37:20 +0200

    refactor: Simplifiy polls state mapping

--- src/components/AnsweredPollList.js
+++ src/components/AnsweredPollList.js
@@ -1,17 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  prop,
-  sortBy,
-  pipe,
-  values,
-  map,
-  contains,
-  where,
-  propSatisfies,
-  reject,
-  complement
-} from 'ramda';
 import {connect} from 'react-redux';
 import AnsweredPoll from './AnsweredPoll';
 const AnsweredPollList = ({questionIds}) => (
@@ -26,23 +14,29 @@ AnsweredPollList.propTypes = {
   questionIds: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
-function mapStateToProps({authedUser, questions}) {
-  const doesntContainUser = complement(contains(authedUser));
-  const optionDoesntContainUser = propSatisfies(doesntContainUser, 'votes');
-  const questionDoesntContainUser = where({
-    optionOne: optionDoesntContainUser,
-    optionTwo: optionDoesntContainUser
-  });
-  const userFilter = reject(questionDoesntContainUser);
-  const sortByTimestamp = sortBy(prop('timestamp'));
+// function mapStateToProps({authedUser, questions}) {
+//   const doesntContainUser = complement(contains(authedUser));
+//   const optionDoesntContainUser = propSatisfies(doesntContainUser, 'votes');
+//   const questionDoesntContainUser = where({
+//     optionOne: optionDoesntContainUser,
+//     optionTwo: optionDoesntContainUser
+//   });
+//   const userFilter = reject(questionDoesntContainUser);
+//   const sortByTimestamp = sortBy(prop('timestamp'));
 
+//   return {
+//     questionIds: pipe(
+//       userFilter,
+//       values,
+//       sortByTimestamp,
+//       map(prop('id'))
+//     )(questions)
+//   };
+// }
+
+function mapStateToProps({users, authedUser}) {
   return {
-    questionIds: pipe(
-      userFilter,
-      values,
-      sortByTimestamp,
-      map(prop('id'))
-    )(questions)
+    questionIds: Object.keys(users[authedUser].answers)
   };
 }
 export default connect(mapStateToProps)(AnsweredPollList);
--- src/components/UnansweredPollList.js
+++ src/components/UnansweredPollList.js
@@ -1,16 +1,5 @@
 import PropTypes from 'prop-types';
-import {
-  complement,
-  contains,
-  filter,
-  map,
-  pipe,
-  prop,
-  propSatisfies,
-  sortBy,
-  values,
-  where
-} from 'ramda';
+import {difference} from 'ramda';
 import React from 'react';
 import {connect} from 'react-redux';
 import UnansweredPoll from './UnansweredPoll';
@@ -26,23 +15,12 @@ UnansweredPollList.propTypes = {
   questionIds: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
-function mapStateToProps({authedUser, questions}) {
-  const doesntContainUser = complement(contains(authedUser));
-  const optionDoesntContainUser = propSatisfies(doesntContainUser, 'votes');
-  const questionDoesntContainUser = where({
-    optionOne: optionDoesntContainUser,
-    optionTwo: optionDoesntContainUser
-  });
-  const userFilter = filter(questionDoesntContainUser);
-  const sortByTimestamp = sortBy(prop('timestamp'));
-
+function mapStateToProps({users, questions, authedUser}) {
   return {
-    questionIds: pipe(
-      userFilter,
-      values,
-      sortByTimestamp,
-      map(prop('id'))
-    )(questions)
+    questionIds: difference(
+      Object.keys(questions),
+      Object.keys(users[authedUser].answers)
+    )
   };
 }
 
