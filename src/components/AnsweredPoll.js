6e7fc241c233a1b5d6d7a2ff0750931c9e1533de
krzysztofwasilewski <krzysztofwasilewski76@gmail.com>
2020-05-14 01:07:22 +0200

    feat: Implement support for answered polls

--- src/components/AnsweredPoll.js
+++ src/components/AnsweredPoll.js
@@ -1,2 +1,58 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {add} from 'ramda';
+import Result from './Result';
+
+const AnsweredPoll = ({poll, author, authedUser}) => {
+  const {optionOne, optionTwo} = poll;
+  const totalVotes = [optionOne, optionTwo]
+    .map(({votes}) => votes.length)
+    .reduce(add);
+  console.log('total votes: ', totalVotes);
+  return (
+    <div>
+      <div>
+        <img src={author.avatarURL} alt={`Avatar of ${author.name}`} />
+      </div>
+      <div>
+        <h2>result</h2>
+        {[optionOne, optionTwo].map(option => (
+          <Result
+            key={option.text}
+            text={option.text}
+            votes={option.votes.length}
+            totalVotes={totalVotes}
+            isMine={option.votes.includes(authedUser)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+AnsweredPoll.propTypes = {
+  author: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    avatarURL: PropTypes.string.isRequired
+  }).isRequired,
+  poll: PropTypes.shape({
+    optionOne: PropTypes.shape({
+      votes: PropTypes.arrayOf(PropTypes.string).isRequired,
+      text: PropTypes.string.isRequired
+    }).isRequired,
+    optionTwo: PropTypes.shape({
+      votes: PropTypes.arrayOf(PropTypes.string).isRequired,
+      text: PropTypes.string.isRequired
+    }).isRequired,
+    author: PropTypes.string.isRequired
+  }).isRequired,
+  authedUser: PropTypes.string.isRequired
+};
+function mapStateToProps({questions, users, authedUser}, {id}) {
+  return {
+    poll: questions[id],
+    author: users[questions[id].author],
+    authedUser
+  };
+}
+export default connect(mapStateToProps)(AnsweredPoll);
--- src/components/AnsweredPollList.js
+++ src/components/AnsweredPollList.js
@@ -1,10 +1,48 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  prop,
+  sortBy,
+  pipe,
+  values,
+  map,
+  contains,
+  where,
+  propSatisfies,
+  reject,
+  complement
+} from 'ramda';
+import {connect} from 'react-redux';
+import AnsweredPoll from './AnsweredPoll';
+const AnsweredPollList = ({questionIds}) => (
+  <div>
+    {questionIds.map(id => (
+      <AnsweredPoll key={id} id={id} />
+    ))}
+  </div>
+);
 
-const AnsweredPollList = () => {
-    return <div>
-        Answered poll List
-    </div>
+AnsweredPollList.propTypes = {
+  questionIds: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+function mapStateToProps({authedUser, questions}) {
+  const doesntContainUser = complement(contains(authedUser));
+  const optionDoesntContainUser = propSatisfies(doesntContainUser, 'votes');
+  const questionDoesntContainUser = where({
+    optionOne: optionDoesntContainUser,
+    optionTwo: optionDoesntContainUser
+  });
+  const userFilter = reject(questionDoesntContainUser);
+  const sortByTimestamp = sortBy(prop('timestamp'));
+
+  return {
+    questionIds: pipe(
+      userFilter,
+      values,
+      sortByTimestamp,
+      map(prop('id'))
+    )(questions)
+  };
 }
-
-export default AnsweredPollList;
+export default connect(mapStateToProps)(AnsweredPollList);
