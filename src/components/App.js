b1be32efd410888b6efd74ea7ca756eabbdd8dd1
krzysztofwasilewski <krzysztofwasilewski76@gmail.com>
2020-05-17 18:14:27 +0200

    fix: Remove unused import form `App.js`

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 
 import {getData} from '../actions/shared';
-import {logIn} from '../actions/authedUser';
+
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from './Header';
