f410bab820002cb613afd4c74994d139c95bc7f6
krzysztofwasilewski <krzysztofwasilewski76@gmail.com>
2020-05-18 21:27:14 +0200

    style: Correct code style in page 404

--- src/components/Page404.js
+++ src/components/Page404.js
@@ -1,8 +1,12 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export const Page404 = ({ location }) => <h1 style={{ marginTop: 200 }}>{`404: Page  ${location.pathname} not found!`}</h1>;
+export const Page404 = ({location}) => (
+  <h1
+    style={{marginTop: 200}}
+  >{`404: Page ${location.pathname} not found!`}</h1>
+);
 
 Page404.propTypes = {
-    location: PropTypes.shape({ pathname: PropTypes.string.isRequired }).isRequired
-}
+  location: PropTypes.shape({pathname: PropTypes.string.isRequired}).isRequired
+};
